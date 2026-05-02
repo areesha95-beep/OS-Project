06c77121754d8de18be1301c8f67b3633579a236
areesha95-beep <areeshanaeem111@gmail.com>
2026-05-03 00:51:52 +0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,29 +1,12 @@
-#include <iostream>
-#include "phase1.h"
-#include "phase2.h"
-#include "phase3.h"
-#include "phase4.h"
-#include "phase5.h"
-#include "phase6.h"
-#include "phase7.h"
-#include "phase8.h"
-#include "phase9.h"
+#include "MyForm.h"
 
-using namespace std;
+using namespace OSProject;
 
-int main() {
-
-    ConfigPhase cp;
-    cp.loadConfig("config.txt");
-    cp.cfg.frameCount = 4;   // force small frame count to show algo differences
-    cp.printConfig();
-
-    SimulatorPhase sim(cp.cfg);
-
-    if (cp.cfg.replacementAlgo == "OPT")
-        sim.preScan("trace.txt");
-
-    sim.run("trace.txt");
-
+[STAThreadAttribute]
+int main()
+{
+    System::Windows::Forms::Application::EnableVisualStyles();
+    System::Windows::Forms::Application::SetCompatibleTextRenderingDefault(false);
+    System::Windows::Forms::Application::Run(gcnew MyForm());
     return 0;
 }
